8c7457495e0161daa1497b4d9a74b13a776970ce
Mehmet Ali KIZILARSLAN <76072213+mmokzlrsn@users.noreply.github.com>
2023-05-01 13:43:18 +0300

    debugging grid values to the grids

--- TurnBasedStrategyProject/Assets/Scripts/Grid/GridSystem.cs
+++ TurnBasedStrategyProject/Assets/Scripts/Grid/GridSystem.cs
@@ -1,32 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Jobs;
 
 public class GridSystem
 {
     private int width;
     private int height;
     private float cellSize;
+
+    private GridObject[,] gridObjectArray; 
     public GridSystem(int width, int height, float cellSize)
     {
         this.width = width;
         this.height = height;
         this.cellSize = cellSize;
 
+        gridObjectArray= new GridObject[width, height]; 
         for (int x = 0;x < width; x++)
         {
             for(int z = 0; z < height; z++)
             {
-                Debug.DrawLine(GetWorldPosition(x,z), GetWorldPosition(x,z)+Vector3.right  * .5f , Color.red, 1000);
+                //Debug.DrawLine(GetWorldPosition(x,z), GetWorldPosition(x,z)+Vector3.right  * .5f , Color.red, 1000);
 
+
+                GridPosition gridPosition = new GridPosition(x,z);
+                gridObjectArray[x,z] =  new GridObject(this, gridPosition);
+
+
+            }
+        }
+    } 
+
+    public void CreateDebugObjects(Transform debugPrefab)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int z = 0; z < height; z++)
+            {
+                GridPosition gridPosition = new GridPosition(x,z);
+
+                Transform debugTransform = GameObject.Instantiate(debugPrefab, GetWorldPosition(gridPosition), Quaternion.identity);
+                GridDebugObject gridDebugObject = debugTransform.GetComponent<GridDebugObject>();
+                gridDebugObject.SetGridObject(GetGridObject(gridPosition));
             }
         }
     }
 
-
-    public Vector3 GetWorldPosition(int x, int z)
+    public GridObject GetGridObject(GridPosition gridPosition)
     {
-        return new Vector3(x, 0, z) * cellSize;
+        return gridObjectArray[gridPosition.x, gridPosition.z];
+    }
+
+
+
+
+    public Vector3 GetWorldPosition(GridPosition gridPosition)
+    {
+        return new Vector3(gridPosition.x, 0, gridPosition.z) * cellSize;
     }
 
     public GridPosition GetGridPosition(Vector3 worldPosition)
--- TurnBasedStrategyProject/Assets/Scripts/Testing.cs
+++ TurnBasedStrategyProject/Assets/Scripts/Testing.cs
@@ -6,10 +6,11 @@ public class Testing : MonoBehaviour
 {
     private GridSystem gridSystem;
     // Start is called before the first frame update
+    [SerializeField] private Transform gridDebugObjectPrefab;
     void Start()
     {
         gridSystem = new GridSystem(10, 10, 2f);
-
+        gridSystem.CreateDebugObjects(gridDebugObjectPrefab);
         Debug.Log(new GridPosition(2, 3));
     }
 
